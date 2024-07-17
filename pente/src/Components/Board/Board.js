28659b28af883611dc837a19908ac4059ad11458
ChelseaBui11 <141456665+ChelseaBui11@users.noreply.github.com>
2024-07-17 08:50:08 -0600

    Board now generates

--- pente/src/Components/Board/Board.js
+++ pente/src/Components/Board/Board.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Square from '../Square/Square';
 
-const Board = () => {
-  return (
-    <div>Board</div>
-  )
+function Board({ squares, onClick }) {
+  const renderSquare = (i) => {
+    return <Square value={squares[i]} onClick={() => onClick(i)} />;
+  };
+
+  const boardSize = 19;
+  const board = Array(boardSize).fill(null).map((_, row) => (
+    <div className="board-row" key={row}>
+      {Array(boardSize).fill(null).map((_, col) => renderSquare(row * boardSize + col))}
+    </div>
+  ));
+
+  return <div>{board}</div>;
 }
 
-export default Board
+export default Board;
