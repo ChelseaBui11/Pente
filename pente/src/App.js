1dc8c8700d907308cf00aaa08cf777b27af6852e
hauntedghoul <madison.mitchell6704@gmail.com>
2024-07-17 08:33:45 -0600

    App.js
    fixed how things were setup so that things actually work :|

--- pente/src/App.js
+++ pente/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import './Components/Game/Game.js'
+import Game from './Components/Game/Game.js';
 
 function App() {
   return (
     <div className="App">
+      <Game/>
     </div>
   );
 }
